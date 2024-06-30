eb6f847d6057e627dedd5b01293083506504bd0d
nartkosova <nartkosova21@gmail.com>
2024-06-30 14:34:16 +0300

    exercise 3.3

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const { time } = require('console')
 const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
+  }
 ]
 
 app.use(express.json())
@@ -38,6 +37,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).send({ error:'person not found'})
+  }
+})
+
 const generateId = () => {
   const maxId = persons.length > 0
     ? Math.max(...persons.map(n => n.id))
@@ -48,32 +57,21 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.name || body.number) {
     return response.status(400).json({ 
-      error: 'content missing' 
+      error: 'name or number missing' 
     })
   }
 
   const person = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
+    name: body.name,
+    number: body.number,
+    id: generateId()
   }
 
   persons = persons.concat(person)
 
   response.json(person)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    console.log('x')
-    response.status(404).end()
-  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
